be1cc3c506130d050729b7b49b77634c639ca5c5
Nakka Sai Preetham <nakkasaipreetham@gmail.com>
2023-04-24 19:48:13 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Sleep for 2 seconds.");
+                Thread.Sleep(2000);
+            }
+
+            Console.WriteLine("Main thread exits.");
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
